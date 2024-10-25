45f0ece094a250ab01e976bb061ea341edd9300f
krisnand2k@gmail.com <krisnand2k@gmail.com>
2024-10-26 01:31:47 +0530

    removed files

--- utiities/data_utilities.py
+++
@@ -1,8 +0,0 @@
-# /path/to/azure/files/dag_folder/utilities/sftp_utilities.py
-
-def python_callable_data(protocol):
-    # Example function logic for SFTP
-    print(f"Connecting to SFTP server: {host_name}:{port} using protocol {protocol}")
-    print(f"Username: {username}")
-    # Add more logic as required
-
